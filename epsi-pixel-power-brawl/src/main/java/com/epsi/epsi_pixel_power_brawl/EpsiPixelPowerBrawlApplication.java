a5c33dcaad16b49bd95738ed8aa31906eb863af7
SuperKiment <kiment2002@gmail.com>
2024-10-04 10:29:12 +0200

    init2

---
+++ epsi-pixel-power-brawl/src/main/java/com/epsi/epsi_pixel_power_brawl/EpsiPixelPowerBrawlApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.epsi_pixel_power_brawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsiPixelPowerBrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsiPixelPowerBrawlApplication.class, args);
+	}
+
+}
---
+++ epsi-pixel-power-brawl/src/test/java/com/epsi/epsi_pixel_power_brawl/EpsiPixelPowerBrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsi.epsi_pixel_power_brawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsiPixelPowerBrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
