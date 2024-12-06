d45b7e0aef9f9511f68aa9f073813b5ff54dd3b2
SuperKiment <kiment2002@gmail.com>
2024-12-06 10:08:05 +0100

    added resistances table

--- pixel-power-ng/src/app/detail-pokemon/detail-pokemon.component.ts
+++ pixel-power-ng/src/app/detail-pokemon/detail-pokemon.component.ts
@@ -11,4 +11,23 @@ import { CommonModule } from '@angular/common';
 })
 export class DetailPokemonComponent {
   @Input() selectedPokemon: Pokemon | null = null;
+
+  getMultiplierColor(multiplier: number): string {
+    switch (multiplier) {
+      case 0:
+        return 'gray';
+      case 0.25:
+        return 'very-green';
+      case 0.5:
+        return 'green';
+      case 1:
+        return '';
+      case 2:
+        return 'red';
+      case 4:
+        return 'very-red';
+    }
+
+    return '';
+  }
 }
