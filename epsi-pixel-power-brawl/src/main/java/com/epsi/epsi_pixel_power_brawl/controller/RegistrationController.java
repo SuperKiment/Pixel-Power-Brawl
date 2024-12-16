a072c2580b299a874bd012361cfaa9223f90077a
Origine0 <antoine244smite@gmail.com>
2024-12-16 13:58:41 +0100

    Reponse JSON RegistrationController

--- epsi-pixel-power-brawl/src/main/java/com/epsi/epsi_pixel_power_brawl/controller/RegistrationController.java
+++ epsi-pixel-power-brawl/src/main/java/com/epsi/epsi_pixel_power_brawl/controller/RegistrationController.java
@@ -1,6 +1,11 @@
 package com.epsi.epsi_pixel_power_brawl.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.epsi.epsi_pixel_power_brawl.dto.LoginDto;
 import com.epsi.epsi_pixel_power_brawl.dto.RegisterDto;
 import com.epsi.epsi_pixel_power_brawl.model.Utilisateur;
 import com.epsi.epsi_pixel_power_brawl.service.UserService;
@@ -29,17 +35,19 @@ public class RegistrationController {
 
 	@PostMapping("/user/registration")
 	@ResponseBody
-	public String registerUserAccount(@RequestBody RegisterDto userDto) {
-	    
+	public ResponseEntity<Map<String, String>> registerUserAccount(@RequestBody RegisterDto userDto) {
+
 	    try {
 	        Utilisateur registered = userService.registerNewUserAccount(userDto);
-	    } catch (Exception uaeEx) {
-	        return "user already exists";
-	    }
+	    } catch (Exception e) {
+	    	Map<String, String> errorResponse = new HashMap<>();
+	        errorResponse.put("error", "User already exists");
+	        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+	    }	    
+	    
+        Map<String, String> response = new HashMap<>();
+        response.put("response", "Registration Successful!");
+        return ResponseEntity.ok(response);
 
-	    return "registration successful!";
 	}
-
-
-
 }
